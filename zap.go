4b938cce9d5ed0ddae5e797b152eb13f7b45d2ed
Fabio Ribeiro <faabiosr@gmail.com>
2022-07-20 00:55:51 +0200

    Uber Zap Logger Middleware
    ZapLog middleware was created to log the information about echo
    HTTP request.

---
+++ zap.go
@@ -0,0 +1,101 @@
+package middleware
+
+import (
+	"github.com/labstack/echo/v4"
+	mw "github.com/labstack/echo/v4/middleware"
+	"go.uber.org/zap"
+)
+
+// ZapLogConfig defines the config for Uber ZapLog middleware.
+type ZapLogConfig struct {
+	// FieldMap set a list of fields with tags
+	//
+	// Tags to constructed the logger fields.
+	//
+	// - @id (Request ID)
+	// - @remote_ip
+	// - @uri
+	// - @host
+	// - @method
+	// - @path
+	// - @protocol
+	// - @referer
+	// - @user_agent
+	// - @status
+	// - @error
+	// - @latency (In nanoseconds)
+	// - @latency_human (Human readable)
+	// - @bytes_in (Bytes received)
+	// - @bytes_out (Bytes sent)
+	// - @header:<NAME>
+	// - @query:<NAME>
+	// - @form:<NAME>
+	// - @cookie:<NAME>
+	FieldMap map[string]string
+
+	// Logger it is a zap logger
+	Logger *zap.Logger
+
+	// Skipper defines a function to skip middleware.
+	Skipper mw.Skipper
+}
+
+// DefaultZapLogConfig is the default Uber ZapLog middleware config.
+var DefaultZapLogConfig = ZapLogConfig{
+	FieldMap: map[string]string{
+		"remote_ip": logRemoteIP,
+		"uri":       logURI,
+		"host":      logHost,
+		"method":    logMethod,
+		"status":    logStatus,
+		"latency":   logLatency,
+		"error":     logError,
+	},
+	Logger: func() *zap.Logger {
+		lg, _ := zap.NewProduction()
+		return lg
+	}(),
+	Skipper: mw.DefaultSkipper,
+}
+
+// ZapLog returns a middleware that logs HTTP requests.
+func ZapLog() echo.MiddlewareFunc {
+	return ZapLogWithConfig(DefaultZapLogConfig)
+}
+
+// ZapLogWithConfig returns a Uber ZapLog middleware with config.
+// See: `ZapLog()`.
+func ZapLogWithConfig(cfg ZapLogConfig) echo.MiddlewareFunc {
+	// Defaults
+	if cfg.Skipper == nil {
+		cfg.Skipper = DefaultZapLogConfig.Skipper
+	}
+
+	if cfg.Logger == nil {
+		cfg.Logger = DefaultZapLogConfig.Logger
+	}
+
+	if len(cfg.FieldMap) == 0 {
+		cfg.FieldMap = DefaultZapLogConfig.FieldMap
+	}
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(ec echo.Context) (err error) {
+			if cfg.Skipper(ec) {
+				return next(ec)
+			}
+
+			zFields := []zap.Field{}
+			logFields, err := mapFields(ec, next, cfg.FieldMap)
+
+			for k, v := range logFields {
+				field := zap.Any(k, v)
+				zFields = append(zFields, field)
+			}
+
+			cfg.Logger.With(zFields...).Info("handle request")
+
+			return
+		}
+	}
+}
