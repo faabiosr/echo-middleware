b549e1ecd0e6b6ee63c04c64faab980431286982
Fabio Ribeiro <faabiosr@gmail.com>
2023-03-19 14:15:19 +0100

    CharmBracelet Logger Middleware
    CharmLog middleware was created to log the information about echo
    HTTP request.

---
+++ charm.go
@@ -0,0 +1,89 @@
+package middleware
+
+import (
+	charm "github.com/charmbracelet/log"
+	"github.com/labstack/echo/v4"
+	mw "github.com/labstack/echo/v4/middleware"
+)
+
+// CharmLogConfig defines the config for CharmBracelet Log middleware.
+type CharmLogConfig struct {
+	// FieldMap set a list of fields with tags
+	//
+	// Tags to constructed the logger fields.
+	//
+	// - @id (Request ID)
+	// - @remote_ip
+	// - @uri
+	// - @host
+	// - @method
+	// - @path
+	// - @protocol
+	// - @referer
+	// - @user_agent
+	// - @status
+	// - @error
+	// - @latency (In nanoseconds)
+	// - @latency_human (Human readable)
+	// - @bytes_in (Bytes received)
+	// - @bytes_out (Bytes sent)
+	// - @header:<NAME>
+	// - @query:<NAME>
+	// - @form:<NAME>
+	// - @cookie:<NAME>
+	FieldMap map[string]string
+
+	// Logger it is a charm logger
+	Logger charm.Logger
+
+	// Skipper defines a function to skip middleware.
+	Skipper mw.Skipper
+}
+
+// DefaultCharmLogConfig is the default CharmBracelet Log middleware config.
+var DefaultCharmLogConfig = CharmLogConfig{
+	FieldMap: defaultFields,
+	Logger:   charm.Default(),
+	Skipper:  mw.DefaultSkipper,
+}
+
+// CharmLog returns a middleware that logs HTTP requests.
+func CharmLog() echo.MiddlewareFunc {
+	return CharmLogWithConfig(DefaultCharmLogConfig)
+}
+
+// CharmLogWithConfig returns a CharmBracelet Log middleware with config.
+// See: `CharmLog()`.
+func CharmLogWithConfig(cfg CharmLogConfig) echo.MiddlewareFunc {
+	// Defaults
+	if cfg.Skipper == nil {
+		cfg.Skipper = DefaultCharmLogConfig.Skipper
+	}
+
+	if cfg.Logger == nil {
+		cfg.Logger = DefaultCharmLogConfig.Logger
+	}
+
+	if len(cfg.FieldMap) == 0 {
+		cfg.FieldMap = DefaultCharmLogConfig.FieldMap
+	}
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(ec echo.Context) (err error) {
+			if cfg.Skipper(ec) {
+				return next(ec)
+			}
+
+			cFields := []interface{}{}
+			logFields, err := mapFields(ec, next, cfg.FieldMap)
+
+			for k, v := range logFields {
+				cFields = append(append(cFields, k), v)
+			}
+
+			cfg.Logger.Info("handle request", cFields...)
+
+			return
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/faabiosr/echo-middleware
 go 1.18
 
 require (
+	github.com/charmbracelet/log v0.1.2
 	github.com/gofrs/uuid/v5 v5.0.0
 	github.com/labstack/echo/v4 v4.10.2
 	github.com/rs/zerolog v1.29.0
@@ -12,11 +13,18 @@ require (
 )
 
 require (
+	github.com/charmbracelet/lipgloss v0.6.0 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/reflow v0.2.1-0.20210115123740-9e1d0d53df68 // indirect
+	github.com/muesli/termenv v0.11.1-0.20220204035834-5ac8409525e0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
