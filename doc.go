5c7e34fe342a970e518f0781d3664a3034c6fe8f
Fabio Ribeiro <faabiosr@gmail.com>
2024-09-05 16:11:08 +0200

    chore(license): add license header

--- doc.go
+++ doc.go
@@ -1,2 +1,7 @@
+/*
+ * Copyright (c) Fabio da Silva Ribeiro <faabiosr@gmail.com>
+ * SPDX-License-Identifier: MIT
+ */
+
 // Package middleware provides middlewares for Echo Framework.
 package middleware
